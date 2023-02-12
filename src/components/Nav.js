d9a60fafd3b670eb17cf58fe3ef4fcc654a7a82d
Alex Frankhouser <freesoul423@gmail.com>
2023-02-12 08:09:06 -0500

    Added nav tabs: About, Contact, Resume, Porfolio with onlick events and started pages folder

---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+    // About Me Section:
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a
+                    href="#about"
+                    onClick={() => handlePageChange("About")}
+                    className={currentPage === "About" ? "nav-link active" : "nav-link"}>
+                    About Me
+                </a>
+            </li>
+
+    {/* Contact Section:  */}
+            <li className="nav-item">
+                <a
+                    href="#contact"
+                    onClick={() => handlePageChange("Contact")}
+                    className={currentPage === "Contact" ? "nav-link active" : "nav-link"}>
+                    Contact
+                </a>
+            </li>
+
+    {/* Portfolio Section:  */}
+            <li className="nav-item">
+                <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange("Portfolio")}
+                    className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}>
+                    Portfolio
+                </a>
+            </li>
+
+    {/* Resume Section:  */}
+                        <li className="nav-item">
+                <a
+                    href="#resume"
+                    onClick={() => handlePageChange("Resume")}
+                    className={currentPage === "Resume" ? "nav-link active" : "nav-link"}>
+                    Resume
+                </a>
+            </li>
+            
+    </ul>
+    );
+}
