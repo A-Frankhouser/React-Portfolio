7366d6bd8ed60608fc25e480d06185ec4f73a3fc
Alex Frankhouser <freesoul423@gmail.com>
2023-02-12 07:49:17 -0500

    setup file structure for components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import bootstrap from  bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
