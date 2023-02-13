27b32e89e7ad98b22cdacc4855fb70c243c8120b
Alex Frankhouser <freesoul423@gmail.com>
2023-02-12 20:17:32 -0500

    added useStates for the form and started working on email or blank  messages errors

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-    const [form]
+    // useState for Contact Form:
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    // useState for error msg for email validation:
+    const [errorMsg, setErrorMsg] = useState("");
+
+    // Values for each part of the form:
+    const { name, email, message } = formState;
+
+    // Function to validate error message and set error
+    function 
 }
