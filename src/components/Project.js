15058635276f784ae37f6fa625dba8cc034f3ff0
Alex Frankhouser <freesoul423@gmail.com>
2023-02-13 12:55:09 -0500

    completed the project card, now adding data for each cards props

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
 
-function Project(
-    // Props:
-    name, 
-    description, 
-    projectImage, 
-    technologies, 
-    deployedURL, 
-    gitHubLink
-    ) {
+function Project(props) {
         // Projects in an array starting from the first one.
     const projectInfo = useState(props)[0].projects;
 
-    const name = projectInfo.name;
+    const projectTitle = projectInfo.projectTitle;
     const description = projectInfo.description;
     const projectImage = projectInfo.projectImage;
     const technologies = projectInfo.technologies;
     const deployedURL = projectInfo.deployedURL;
     const gitHubLink = projectInfo.gitHubLink;
+
+    return(
+        // Project Card:
+        <Card>
+                {/* Card Image: */}
+            <div className="card" style={'width: 18rem'}>
+                <img src={require(`../assets/${projectImage}`)} className="card-img-top" alt={projectTitle}/>
+                {/* Project title and description: */}
+                <div className="card-body">
+                    <h5 className="card-title">{projectTitle}</h5>
+                    <p className="card-text">{description}</p>
+                </div>
+                {/* Technologies used: */}
+                <div className="card-body">
+                    <h5 className="card-title">Skills used:</h5>
+                    <p className="card-text">{technologies}</p>
+                </div>
+                {/* Links to github and deployed site: */}
+                <div className="card-body">
+                    <a href={deployedURL}><img src={require('../assets/deploy-icon.png')} alt='Deployed Icon' id="card-icon"/></a>
+                    <a href={gitHubLink}><img src={require('../assets/github-mark-white.png')} alt='GitHub Icon' id="card-icon"/></a>
+                </div>
+            </div>
+        </Card>
+    );
 }
 
 export default Project;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,15 +1,58 @@
 import React from "react";
 import Project from "../Project";
 
-function Portfolio( 
-    name, 
-    description, 
-    projectImage, 
-    technologies, 
-    deployedURL, 
-    gitHubLink
-    ) {
-        
+function Portfolio() {
+    // Projects Info:
+    const projects = [
+        {
+            projectTitle: 'The Blog Spot',
+            description: '',
+            image: '',
+            technologies:'',
+            deployedURL: 'https://the-blog-spotv1.herokuapp.com/',
+            gitHubLink: 'https://github.com/A-Frankhouser/Blog-Site'
+        },
+        {
+            projectTitle: '',
+            description: '',
+            image: '',
+            technologies:'',
+            deployedURL: 'https://the-blog-spotv1.herokuapp.com/',
+            gitHubLink: 'https://github.com/A-Frankhouser/Blog-Site'
+        },
+        {
+            projectTitle: '',
+            description: '',
+            image: '',
+            technologies:'',
+            deployedURL: 'https://the-blog-spotv1.herokuapp.com/',
+            gitHubLink: 'https://github.com/A-Frankhouser/Blog-Site'
+        },
+        {
+            projectTitle: '',
+            description: '',
+            image: '',
+            technologies:'',
+            deployedURL: 'https://the-blog-spotv1.herokuapp.com/',
+            gitHubLink: 'https://github.com/A-Frankhouser/Blog-Site'
+        },
+        {
+            projectTitle: '',
+            description: '',
+            image: '',
+            technologies:'',
+            deployedURL: 'https://the-blog-spotv1.herokuapp.com/',
+            gitHubLink: 'https://github.com/A-Frankhouser/Blog-Site'
+        },
+        {
+            projectTitle: '',
+            description: '',
+            image: '',
+            technologies:'',
+            deployedURL: 'https://the-blog-spotv1.herokuapp.com/',
+            gitHubLink: 'https://github.com/A-Frankhouser/Blog-Site'
+        }
+    ]
     }
 
 export default Portfolio;
